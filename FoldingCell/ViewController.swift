3272bf40fa66f26b96aead9f9f4448d963f663ca
jason <jasonlinscombe@gmail.com>
2019-09-04 23:23:48 +0200

    Initial Commit

---
+++ FoldingCell/ViewController.swift
@@ -0,0 +1,146 @@
+//
+//  ViewController.swift
+//  FoldingCell
+//
+//  Created by Jason Linscombe on 5/14/19.
+//  Copyright © 2019 Give Me Some Coffee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    let cellReuseIdentifier = "FoldingCell"
+
+    enum Const {
+        static let closeCellHeight: CGFloat = 116
+        static let openCellHeight: CGFloat = 316
+        static let rowsCount = 10
+    }
+    
+    var cellHeights: [CGFloat] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
+        cellHeights = Array(repeating: Const.closeCellHeight, count: Const.rowsCount)
+        tableView.estimatedRowHeight = Const.closeCellHeight
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.backgroundColor = UIColor(ciColor: .white)
+        if #available(iOS 10.0, *) {
+            tableView.refreshControl = UIRefreshControl()
+            tableView.refreshControl?.addTarget(self, action: #selector(refreshHandler), for: .valueChanged)
+        }
+    }
+    
+    @objc func refreshHandler() {
+        let deadlineTime = DispatchTime.now() + .seconds(1)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: { [weak self] in
+            if #available(iOS 10.0, *) {
+                self?.tableView.refreshControl?.endRefreshing()
+            }
+            self?.tableView.reloadData()
+        })
+    }
+}
+
+extension ViewController {
+
+    override func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard case let cell as DemoCell = cell else {
+            return
+        }
+        
+        cell.backgroundColor = .clear
+        
+        if cellHeights[indexPath.row] == Const.closeCellHeight {
+            cell.unfold(false, animated: false, completion: nil)
+        } else {
+            cell.unfold(true, animated: false, completion: nil)
+        }
+        
+        cell.number = indexPath.row
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! FoldingCell
+        let durations: [TimeInterval] = [0.26, 0.2, 0.2]
+        cell.durationsForExpandedState = durations
+        cell.durationsForCollapsedState = durations
+        return cell
+    }
+    
+    override func tableView(_: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return cellHeights[indexPath.row]
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let cell = tableView.cellForRow(at: indexPath) as! FoldingCell
+        
+        if cell.isAnimating() {
+            return
+        }
+        
+        var duration = 0.0
+        let cellIsCollapsed = cellHeights[indexPath.row] == Const.closeCellHeight
+        if cellIsCollapsed {
+            cellHeights[indexPath.row] = Const.openCellHeight
+            cell.unfold(true, animated: true, completion: nil)
+            duration = 0.5
+        } else {
+            cellHeights[indexPath.row] = Const.closeCellHeight
+            cell.unfold(false, animated: true, completion: nil)
+            duration = 0.8
+        }
+        
+        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: { () -> Void in
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            
+            // fix https://github.com/Ramotion/folding-cell/issues/169
+            if cell.frame.maxY > tableView.frame.maxY {
+                tableView.scrollToRow(at: indexPath, at: UITableView.ScrollPosition.bottom, animated: true)
+            }
+        }, completion: nil)
+    }
+}
+
+class DemoCell: FoldingCell {
+    
+    @IBOutlet var closeNumberLabel: UILabel!
+    @IBOutlet var openNumberLabel: UILabel!
+    
+    var number: Int = 0 {
+        didSet {
+            closeNumberLabel.text = String(number)
+            openNumberLabel.text = String(number)
+        }
+    }
+    
+    override func awakeFromNib() {
+        foregroundView.layer.cornerRadius = 10
+        foregroundView.layer.masksToBounds = true
+        super.awakeFromNib()
+    }
+    
+    override func animationDuration(_ itemIndex: NSInteger, type _: FoldingCell.AnimationType) -> TimeInterval {
+        let durations = [0.26, 0.2, 0.2]
+        return durations[itemIndex]
+    }
+}
+
+// MARK: - Actions ⚡️
+extension DemoCell {
+    
+    @IBAction func buttonHandler(_: AnyObject) {
+        print("tap")
+    }
+}
